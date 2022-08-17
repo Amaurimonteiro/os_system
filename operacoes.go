128a1c847a5095284be6fb6e4aa0461303edf0f1
Amauri <70905111+Amaurimonteiro@users.noreply.github.com>
2022-08-17 13:26:03 -0300

    Create operacoes.go

---
+++ operacoes.go
@@ -0,0 +1,14 @@
+package matematica
+
+// Somar devolve a soma de dois Numeros
+func Somar(a int, b int) int {
+	return a + b
+}
+
+func Divide(n1 int, n2 int) int {
+	return n1 / n2
+}
+
+func Mult(n1 int, n2 int) int {
+	return n1 * n2
+}
