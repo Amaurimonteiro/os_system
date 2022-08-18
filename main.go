4d719c702dd3493d8449af9c67abdbcaa345d260
Amauri Soares Monteiro <leidincomp@gmail.com>
2022-08-18 06:38:00 -0300

    sd

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	fmt.Println("Hello!")
 	a := "Amauri"
 	fmt.Println(a)
-	fmt.Println(matematica.Soma(1, 1))
+	fmt.Println(S(1, 1))
 
 	// teste
 	// Mais uma alteração
--- matematica/matematica.go
+++ matematica/matematica.go
@@ -3,13 +3,11 @@ package matematica
 // Somar devolve a soma de dois Numeros
 func Somar(a int, b int) int {
 	return a + b
-
 }
 
 //Div divide dois numeros
 func Div(n1 int, n2 int) int {
 	return n1 / n2
-
 }
 
 //Mult multiplica dois numeros
