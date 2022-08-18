c20a5f524a221b9188ff013fd0c955eb153c4b3f
Amauri Soares Monteiro <leidincomp@gmail.com>
2022-08-18 06:07:16 -0300

    fg

--- operacoes.go
+++ operacoes.go
@@ -3,12 +3,15 @@ package matematica
 // Somar devolve a soma de dois Numeros
 func Somar(a int, b int) int {
 	return a + b
+
 }
 
+//Divide divide dois numeros
 func Divide(n1 int, n2 int) int {
 	return n1 / n2
 }
 
+//Mult multiplica dois numeros
 func Mult(n1 int, n2 int) int {
 	return n1 * n2
 }
