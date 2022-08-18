a922268e2b260793a6d0b40117e4a1756ab8b312
Amauri Soares Monteiro <leidincomp@gmail.com>
2022-08-18 06:33:18 -0300

    fg

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello!")
+	a := "Amauri"
+	fmt.Println(a)
+	fmt.Println(matematica.Soma(1, 1))
+
+	// teste
+	// Mais uma alteração
+	//
+}
--- matematica/operacoes.go
+++ matematica/operacoes.go
@@ -6,10 +6,10 @@ func Somar(a int, b int) int {
 
 }
 
-//Divide divide dois numeros
-func Divide(n1 int, n2 int) int {
+//Div divide dois numeros
+func Div(n1 int, n2 int) int {
 	return n1 / n2
-	
+
 }
 
 //Mult multiplica dois numeros
